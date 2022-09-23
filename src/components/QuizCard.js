2565c0ae729248411c2d31e7aa3a87c8de1a0856
chimsara <ericanwoga@gmail.com>
2022-09-22 21:57:41 -0400

    deploying as website

--- src/components/QuizCard.js
+++ src/components/QuizCard.js
@@ -1,8 +1,18 @@
+import { useState, useEffect } from 'react';
 import '../styles/Quiz.css';
 
-function QuizCard(quizQuestions) {
+function QuizCard({quizQuestions}) {
+    let blah = "Clown";
+
+    function funcX () {
+        document.getElementById("texttt").innerHTML = "bye" ;
+    }
+
+
+
     return ( 
         <div className='quiz-card-box'>
+            <h1 id="texttt">Hello</h1>
         </div>
      );
 }
