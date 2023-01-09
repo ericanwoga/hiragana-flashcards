d9184f09efeb388e2214f3a4065093f09c2494b8
chimsara <ericanwoga@gmail.com>
2023-01-09 00:46:33 -0500

    Styling quiz page

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -5,21 +5,25 @@ import '../styles/Quiz.css';
 
 function Quiz() {
     const {setPage, numOfQuestions, questionList} = useContext(StartMenuContext);
-    const [currentQuestion, setCurrentQuestion] = useState(1);
+    const [currentQuestion, setCurrentQuestion] = useState(() => 1);
 
     return (
         <div className="Quiz">
             <div className='quiz-content'>
-                <div className="entire-title">
+                <div className="quiz-title-box">
                     <h1 id="quiz-title">Question {currentQuestion}/{numOfQuestions}</h1>
                     <h2 id="quiz-subtitle">what is this character?</h2>
-                    <div className='card-and-input'>
-                        <div className="quiz-card-box">
-                            {questionList[currentQuestion - 1].hiragana}
-                        </div>
-                        
-                        <input type="text" placeholder='answer'></input>
-                        
+                </div>
+                <div className='card-input-nav'>
+                    <div className="quiz-card-box">
+                        {questionList[currentQuestion - 1].hiragana}
+                    </div>
+                    
+                    <input className="quiz-card-input" type="text" placeholder='answer'></input>
+                    
+                    <div className='quiz-nav'>
+                        <button>prev</button>
+                        <button>next</button>
                     </div>
                 </div>
             </div>
